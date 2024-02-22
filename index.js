4b9d3373c1c1e9c1d901811b4d87b8859a5b8145
AdilCheema07 <adcheema85@gmail.com>
2024-02-22 20:58:31 +0500

    Starting Footer

--- index.js
+++ index.js
@@ -21,3 +21,32 @@ gridBox.addEventListener('mouseout', function() {
     this.querySelector('.grid-box-title').style.color = 'black';
   });
 });
+
+
+const counterUp = window.counterUp.default
+
+        const callback = entries => {
+            entries.forEach(entry => {
+                const el = entry.target
+                if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+                    counterUp(el, {
+                        duration: 2000,
+                        delay: 16,
+                    })
+                    el.classList.add('is-visible')
+                }
+            })
+        }
+
+        const IO = new IntersectionObserver(callback, { threshold: 1 })
+
+        let el = document.querySelector('.counter')
+        let al = document.querySelector('.counter1')
+        let sl = document.querySelector('.counter2')
+        let dl = document.querySelector('.counter3')
+        let hl = document.querySelector('.counter4')
+        IO.observe(el)
+        IO.observe(al)
+        IO.observe(sl)
+        IO.observe(dl)
+        IO.observe(hl)
