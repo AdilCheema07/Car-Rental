e02797e3e07e360229996a86a03c535f9ad0a126
AdilCheema07 <adcheema85@gmail.com>
2024-02-22 14:49:14 +0500

    Making Global Network Section

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ function toggleMobileMenu(){
     if (menu.style.display === 'none') {
         menu.style.display='block';
         
-    }else{
+    }
+    else{
         menu.style.display='none';
     }
 }
+
+let gridBox = document.querySelectorAll('.grid-box')
+gridBox.forEach(function(gridBox){
+
+
+gridBox.addEventListener('mouseover', function(){
+    this.querySelector('.grid-box-title').style.color = '#FAB700';
+})
+gridBox.addEventListener('mouseout', function() {
+    // Change background color back to blue when mouse moves out
+    this.querySelector('.grid-box-title').style.color = 'black';
+  });
+});
